9a00add326847f73f4144f9db9aa4ad56a2ea512
Daniel Pagan <deapagan@gmail.com>
2022-12-01 10:24:49 -0500

    Day 1 solution

---
+++ day1/Program.fs
@@ -0,0 +1,27 @@
+﻿module day1
+
+let inputFile = "./input.txt"
+
+let getInput =
+    System.IO.File.ReadAllLines(inputFile)
+
+let elfPacksHelper =
+    Array.fold (fun acc calorie -> 
+        if calorie = "" then 
+            (0, (acc |> fst) :: (acc |> snd))
+        else
+            ((acc |> fst) + (calorie |> int),
+            acc |> snd)
+    ) (0, List.Empty)
+
+let elfPacks = elfPacksHelper >> snd
+
+[<EntryPoint>]
+let main argv =
+    let lines = getInput
+    let packs = lines |> elfPacks
+    let maxPack = packs |> List.max
+    let topThreePacks = packs |> List.sort |> List.rev |> List.take 3
+    printfn "%d" maxPack
+    printfn "%d" (List.sum topThreePacks)
+    0
