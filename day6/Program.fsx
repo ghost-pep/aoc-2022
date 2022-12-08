9be1cf61b3ef5ccaa699f8464f3f024d9a09e616
Daniel Pagan <deapagan@gmail.com>
2022-12-07 19:01:42 -0500

    day6

---
+++ day6/Program.fsx
@@ -0,0 +1,22 @@
+﻿let input =
+    System
+        .IO
+        .File
+        .ReadAllText($"{__SOURCE_DIRECTORY__}/input.txt")
+        .Trim()
+
+let test = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+
+let msgIdx len (msg: string) =
+    msg
+    |> Seq.mapi (fun i _ ->
+        msg[i .. msg.Length]
+        |> Seq.chunkBySize len
+        |> Seq.head)
+    |> Seq.findIndex (fun s ->
+        let n_unique = s |> Seq.distinct |> Seq.length
+        n_unique = len)
+    |> (fun n -> n + len)
+
+input |> msgIdx 4
+input |> msgIdx 14
