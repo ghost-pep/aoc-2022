72f30994ae95c231a1a9f07996ff384f0dd5eb07
Daniel Pagan <deapagan@gmail.com>
2022-12-13 21:40:47 -0500

    day10

--- day10/Program.fsx
+++ day10/Program.fsx
@@ -5,6 +5,13 @@
         .ReadAllText($"{__SOURCE_DIRECTORY__}/input.txt")
         .Trim()
 
+let test =
+    System
+        .IO
+        .File
+        .ReadAllText($"{__SOURCE_DIRECTORY__}/test.txt")
+        .Trim()
+
 let (|Prefix|_|) (p: string) (s: string) =
     if s.StartsWith(p) then
         Some(s.Substring(p.Length))
@@ -16,7 +23,13 @@ type Instruction =
     | Add of int
     | Add2 of int
 
-type State = { X: int }
+type CRT = { Buffer: char array; CurIdx: int }
+
+let emptyCRT =
+    { Buffer = Array.create 240 ' '
+      CurIdx = 0 }
+
+type State = { X: int; Monitor: CRT }
 
 let parseLine (line: string) =
     match line with
@@ -28,17 +41,32 @@ let parse (input: string) =
     |> List.ofArray
     |> List.map parseLine
 
+let updateCRT state =
+    let curBuffer = Array.copy state.Monitor.Buffer
+    let curIdx = state.Monitor.CurIdx
+    let curXIdx = curIdx % 40
+    let curX = state.X
+
+    if curXIdx >= curX - 1 && curXIdx <= curX + 1 then
+        curBuffer.SetValue('#', curIdx)
+    else
+        curBuffer.SetValue('.', curIdx)
+
+    { Buffer = curBuffer
+      CurIdx = curIdx + 1 }
+
 let rec runInstruction states instruction =
+    let curState = List.head states
+    let newState = { curState with Monitor = updateCRT curState }
+
     match instruction with
-    | Noop -> List.head states :: states
-    | Add x -> runInstruction (List.head states :: states) (Add2 x)
-    | Add2 x ->
-        let newState = List.head states
-        { newState with X = newState.X + x } :: states
+    | Noop -> newState :: states
+    | Add x -> runInstruction (newState :: states) (Add2 x)
+    | Add2 x -> { newState with X = newState.X + x } :: states
 
 let run instructions =
     instructions
-    |> List.fold runInstruction [ { X = 1 } ]
+    |> List.fold runInstruction [ { X = 1; Monitor = emptyCRT } ]
 
 let sumStates (states: State list) =
     let interestingIdxs = [ 20; 60; 100; 140; 180; 220 ]
@@ -46,10 +74,18 @@ let sumStates (states: State list) =
 
     interestingIdxs
     |> List.map (fun idx -> List.tryItem (idx - 1) reversed) // subtract by 1 for 0 based indexing
-    |> List.map (Option.defaultValue { X = 0 })
+    |> List.map (Option.defaultValue { X = 0; Monitor = emptyCRT })
     |> List.map (fun s -> s.X)
     |> List.zip interestingIdxs
     |> List.map (fun (cycle, x) -> cycle * x)
     |> List.sum
 
-parse input |> run |> sumStates
+let visualizeCRT state =
+    for y in 0..5 do
+        for x in 0..39 do
+            let idx = x + (y * 40)
+            printf "%c" state.Monitor.Buffer[idx]
+
+        printf "\n"
+
+parse input |> run |> List.head |> visualizeCRT
